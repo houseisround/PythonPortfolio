aaeaf68737e571b79c94d6c91c110cf0059c23f3
houseisround <106474124+houseisround@users.noreply.github.com>
2024-01-08 11:11:49 +0400

    Create TelebotOleg.py

---
+++ TelebotOleg.py
@@ -0,0 +1,16 @@
+import telebot
+bot = telebot.TeleBot('6461382927:AAEZVh1Z81lZHRCYMQ3h788CRVHC6jA9ZIg')
+
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
